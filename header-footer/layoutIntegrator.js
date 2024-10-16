c31b07d726e33b7d63bf49b63da63968fac7b196
AnthonyTcha <anthonytcha123@gmail.com>
2024-10-16 14:28:10 +0200

    [Updated] Hid unused domains sectors

--- header-footer/layoutIntegrator.js
+++ header-footer/layoutIntegrator.js
@@ -11,22 +11,8 @@ class CustomHeader extends HTMLElement {
                         Uncomment below if you prefer to use an image logo -->
                     <a href="../index.html"><img src="../assets/img/logo.png" alt="Logo de GreenTwins" class="img-fluid"></a>
                 </div>
-        
                 <nav id="navbar" class="navbar order-last order-lg-0">
                     <ul>
-                        <li class="dropdown"><a href="#"><span>Produits</span> <i class="bi bi-chevron-down"></i></a>
-                            <ul>
-                                <li><a class="nav-link scrollto" href="#">Dashboard & suivi de consommation</a></li>
-                                <li><a class="nav-link scrollto" href="#">Audit énergétique</a></li>
-                                <li><a class="nav-link scrollto" href="#">Bilan carbone</a></li>
-                                <li><a class="nav-link scrollto" href="#">Cartographie d'énergie</a></li>
-                                <li><a class="nav-link scrollto" href="#">Subvention</a></li>
-                                <li><a class="nav-link scrollto" href="#">Financement des travaux</a></li>
-                                <li><a class="nav-link scrollto" href="#">Digitalisation</a></li>
-                                <li><a class="nav-link scrollto" href="#">Data & IA</a></li>
-                                <li><a class="nav-link scrollto" href="#">ISO 51001</a></li>
-                            </ul>
-                        </li>
                         <li class="dropdown"><a href="../technologies-pages/technologies-landing.html" class="${currentPage.includes('technologies-') ? 'current-page' : ''}"><span>Technologies</span> <i class="bi bi-chevron-down"></i></a>
                             <ul>
                                 <li class="mobile-section-link"> <a href="../technologies-pages/technologies-landing.html">Présentation</a></li>
@@ -42,19 +28,8 @@ class CustomHeader extends HTMLElement {
                         <li class="dropdown"><a href="../domaines-pages/domaines-landing.html" class="${currentPage.includes('domaines-') ? 'current-page' : ''}"><span>Domaines</span> <i class="bi bi-chevron-down"></i></a>
                             <ul>
                                 <li class="mobile-section-link"> <a href="../domaines-pages/domaines-landing.html">Présentation</a></li>
-                                
                                 <li><a href="../domaines-pages/domaines-smart-building.html">Smart Building</a></li>
                                 <li><a href="../domaines-pages/domaines-methanisation.html">Méthanisation</a></li>
-                                <li><a href="#">Vidéo</a></li>
-                                <li><a href="#">Article</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown"><a href="#"><span>Actualités</span> <i class="bi bi-chevron-down"></i></a>
-                            <ul>
-                                <li><a class="nav-link scrollto" href="#">@Télévision</a></li>
-                                <li><a class="nav-link scrollto" href="#">Facebook</a></li>
-                                <li><a class="nav-link scrollto" href="#">Twitter</a></li>
-                                <li><a class="nav-link scrollto" href="#">LinkedIn</a></li>
                             </ul>
                         </li>
                         <li class="dropdown"><a href="https://green-twins-web-app.vercel.app"><span><strong>Espace client</strong></span>
@@ -86,28 +61,11 @@ class CustomFooter extends HTMLElement {
                                 <a href="../index.html"><img src="../assets/img/logo-white.png" alt="Logo de GreenTwins" class="img-fluid"></a>
         
                                 <div class="social-links mt-3">
-                                    <a href="#" class="tv"><i class="bi bi-tv"></i></a>
-                                    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                                    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                                    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                                    <a href="https://fr.linkedin.com/company/greentwins" class="linkedin"><i class="bx bxl-linkedin"></i></a>
                                 </div>
                             </div>
                         </div>
         
-                        <div class="col-lg-2 col-md-6 footer-links">
-                            <h4>Produits</h4>
-                            <ul>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Dashboard & suivi de consommation</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Audit énergétique</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Bilan carbone</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Cartographie d'énergie</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Subvention</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Financement des travaux</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Digitalisation</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Data & IA</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">ISO 51001</a></li>
-                            </ul>
-                        </div>
          
                         <div class="col-lg-2 col-md-6 footer-links">
                             <h4><a href="../technologies-pages/technologies-landing.html">Technologies</a></h4>
@@ -126,18 +84,11 @@ class CustomFooter extends HTMLElement {
                             <ul>
                                 <li><i class="bx bx-chevron-right"></i> <a href="../domaines-pages/domaines-smart-building.html">Smart Building</a></li>
                                 <li><i class="bx bx-chevron-right"></i> <a href="../domaines-pages/domaines-methanisation.html">Méthanisation</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Vidéo</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Article</a></li>
                             </ul>
                         </div>
         
                         <div class="col-lg-2 col-md-6 footer-links">
-                            <h4>Espace client</h4>
-                            <ul>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Industriel</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Bureau d'études</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Collectivité</a></li>
-                            </ul>
+                            <h4><a href="https://green-twins-web-app.vercel.app/">Espace client</a></h4>
                         </div>
         
                         <div class="col-lg-2 col-md-6 footer-links">
@@ -171,7 +122,7 @@ class CustomFooter extends HTMLElement {
                         <!-- You can delete the links only if you purchased the pro version. -->
                         <!-- Licensing information: https://bootstrapmade.com/license/ -->
                         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/ -->
-                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                        <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
                 </div>
             </div>
         </footer>
